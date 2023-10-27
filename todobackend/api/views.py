07c944b9cff9bf7c9719d3bdc1a86c8e24f94f43
OllyYa <exrhodo@gmail.com>
2023-10-27 17:29:30 +0300

    CRUD and permissions

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -1,5 +1,5 @@
-from rest_framework import generics
-from .serializers import TodoSrializer
+from rest_framework import generics, permissions
+from .serializers import TodoSrializer, TodoToggleCompleteSerializer
 from todo.models import Todo
 
 
@@ -7,6 +7,7 @@ class TodoListCreate(generics.ListCreateAPIView):
     # ListApiView requires teo mandatory attributes, serializer_class and queryset.
     # We specify TodoSerializer which we have earlier implemented
     serializer_class = TodoSrializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
@@ -16,3 +17,26 @@ class TodoListCreate(generics.ListCreateAPIView):
         """ Function that provides user-id into a todo-model to create todo """
         # serializer holds a django model
         serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSrializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # user can only update, delete own posts
+        return Todo.objects.filter(user=user)
+
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.completed = not (serializer.instance.completed)
+        serializer.save()
